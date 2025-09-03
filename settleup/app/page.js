7d97b16b6bcb58cc35bc88758c8f4a70c6821cb4
sawaraunakk31 <sawaraunakk31@gmail.com>
2025-09-03 23:51:31 +0530

    initial setup

--- settleup/app/page.js
+++ settleup/app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Raunak Sawa</div>
+    <div>Raunak Sawa
+    <br/>
+    <Button>Button</Button>
+    </div>
   );
 }
