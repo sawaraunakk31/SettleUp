18bca9ee514429ecd4b6957b0f803fed7d135719
sawaraunakk31 <sawaraunakk31@gmail.com>
2025-09-05 01:10:54 +0530

    basic clerk and convex setup

--- settleup/app/layout.js
+++ settleup/app/layout.js
@@ -1,16 +1,10 @@
-import localFont from "next/font/local";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter=Inter({subsets:["latin"]});
 
 export const metadata = {
   title: "SettleUp",
@@ -21,9 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+        <ConvexClientProvider>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        </ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
